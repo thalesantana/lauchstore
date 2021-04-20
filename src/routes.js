c53146a11324ae1049fae856fa15892194e3ad3a
Thales <thales.c.santana@outlook.com>
2021-04-20 02:19:47 -0300

    feat: search-bar

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,13 @@ const multer = require('./app/middlewares/multer')
 
 const productController = require('./app/controllers/productController')
 const HomeController = require('./app/controllers/HomeController')
-
-
+const SearchController = require('./app/controllers/SearchController')
+//Home
 routes.get('/',HomeController.index);
 
+// Search
+routes.get('/products/search', SearchController.index)
+//Products
 routes.get('/products/create', productController.create);
 routes.get('/products/:id', productController.show)
 routes.get('/products/:id/edit', productController.edit)
@@ -16,10 +19,13 @@ routes.post('/products',multer.array("photos", 6), productController.post)
 routes.put('/products',multer.array("photos", 6), productController.put)
 routes.delete('/products', productController.delete)
 
+
+
 // Alias
 routes.get('/ads/create', function(req,res){
     return res.redirect("/products/create")
 });
+
 
 
 module.exports = routes;
